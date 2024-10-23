c9c62cdfc0e87e51d22e18707159d5d38fff0ddf
Franze <diazfranzpeter@gmail.com>
2024-10-24 02:19:40 +0800

    Update same_site configuration in session.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -52,7 +52,7 @@ class AuthController extends Controller
         $user = $request->user();
         $token = $user->createToken('token')->plainTextToken;
 
-        $cookie = Cookie::make('jwt', $token, 60 * 24);
+        $cookie = Cookie::make('jwt', $token, 60 * 24, '/', null, true, true, false, 'None');
 
         return response(['message' => $user])->withCookie($cookie);
     }
