2355250347861252d9530ad66562036f0531c8a0
Franze <diazfranzpeter@gmail.com>
2024-11-15 21:16:22 +0800

    Refactor Due Activity Reminder email styles for improved layout and responsiveness, and update CSS for better presentation

--- resources/views/emails/due-activity-reminder.blade.php
+++ resources/views/emails/due-activity-reminder.blade.php
@@ -5,7 +5,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/due-reminder.css')}}">
     <style>
         
 *::after, *::before, * {
@@ -33,7 +32,7 @@
     gap: .5rem;
 }
 .logo {
-    font-size: 1.1rem;
+    font-size: 1rem;
     text-align: center;
 }
 .cardHeader {
