37795c40705c156726b20b1ac3cb143f98900a56
Franze <diazfranzpeter@gmail.com>
2024-11-15 21:45:05 +0800

    Remove unused font and CSS links from Due Activity Reminder email template

--- resources/views/emails/due-activity-reminder.blade.php
+++ resources/views/emails/due-activity-reminder.blade.php
@@ -2,10 +2,6 @@
 <html>
 <head>
     <title>Test Email</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/due-reminder.css')}}">
     <style>
         .contaner {
             width: 100%;
